bb8612053d785ac29397ef3381744e02a3863cfd
eyramakua <eyramagginie@gmail.com>
2024-01-31 10:09:14 -0800

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,20 +1,33 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a tree
- * @tree: pointer to the root
- * Return:height of the binary tree
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node
+ * Return: 1 if node, 0 if otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL && node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node
+ * Return: the height of the tree. If the tree is NULL, return 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left, right;
 
-	if (tree == NULL)
-		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height) ? left_height + 1 : right_height + 1);
+	if (tree == NULL || binary_tree_is_leaf(tree))
+			return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left >= right)
+		return (1 + left);
+	return (1 + right);
 }
 
 /**
@@ -24,17 +37,21 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	binary_tree_t *l, *r;
+
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height == right_height)
+	l = tree->left;
+	r = tree->right;
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	if (l == NULL || r == NULL)
+		return (0);
+	if (binary_tree_height(l) == binary_tree_height(r))
 	{
-		return (binary_tree_is_perfect(tree->left) &&
-				binary_tree_is_perfect(tree->right));
+		if (binary_tree_is_perfect(l) && binary_tree_is_perfect(r))
+			return (1);
 	}
-
 	return (0);
 }
