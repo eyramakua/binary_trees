62245d8afcb55c2e94b459100b2f982df0042da3
eyramakua <eyramagginie@gmail.com>
2024-02-01 04:09:26 -0800

    Task 27

--- binary_trees.h
+++ binary_trees.h
@@ -56,6 +56,9 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+
 
 
 #endif
