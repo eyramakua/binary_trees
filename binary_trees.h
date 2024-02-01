ca73086727dda1b7851f7079b0f8de74a4c1627a
eyramakua <eyramagginie@gmail.com>
2024-02-01 06:35:54 -0800

    124-sorted_array_to_avl.c

--- binary_trees.h
+++ binary_trees.h
@@ -60,6 +60,7 @@ bst_t *bst_search(const bst_t *tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 
 
 #endif
